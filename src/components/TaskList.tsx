91b8eb779ec684d93db15574b2fde212e737fb36
Jessica Pires <pires.jessik@gmail.com>
2022-02-22 19:48:41 -0300

    update

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,6 +16,15 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if(!newTaskTitle) return;
+
+    const newTask ={
+      id: Math.random(),
+      title:newTaskTitle,
+      isComplete: false,
+    }
+    setTasks(oldState =>[...oldState, newTask]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
@@ -24,6 +33,8 @@ export function TaskList() {
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const filteredTasks = tasks.filter(task =>task.id !==id);
+    setTasks(filteredTasks)
   }
 
   return (
